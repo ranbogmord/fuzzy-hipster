84ed5801e8c115d23d40e03b2140aa1e4243d763
John Eriksson <john@provideit.se>
2014-09-22 15:20:42 +0200

    enabled ms

--- wp-config.php
+++ wp-config.php
@@ -48,11 +48,12 @@ define( 'WP_CONTENT_DIR', dirname(__FILE__) . '/wp-content' );
 
 /* Multisite. */
 define( 'WP_ALLOW_MULTISITE', true );
-// define('SUBDOMAIN_INSTALL', true);
-// define('DOMAIN_CURRENT_SITE', 'vps1.capi');
-// define('PATH_CURRENT_SITE', '/');
-// define('SITE_ID_CURRENT_SITE', 1);
-// define('BLOG_ID_CURRENT_SITE', 1);
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', true);
+define('DOMAIN_CURRENT_SITE', 'vps1.capi');
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
 
 
 /* WordPress debug mode for developers. */
