7df2d13f7adbddd409ff39d62df80495671189cd
John Eriksson <john@provideit.se>
2014-09-22 16:29:14 +0200

    Update with shared files

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :deploy_to, '/var/www/capistrano'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{wp-config.php .htaccess}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
